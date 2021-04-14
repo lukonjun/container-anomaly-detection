4bfcb545f6a0c05eb4a318dbe08983e94679635a
Lucas Dario Stocksmeier <l.stocksmeier@campus.tu-berlin.de>
2021-04-14 20:18:09 +0200

    blank project metrics collector

---
+++ metrics-collector/src/main/java/de/lukonjun/metricscollector/MetricsCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.lukonjun.metricscollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsCollectorApplication.class, args);
+	}
+
+}
---
+++ metrics-collector/src/test/java/de/lukonjun/metricscollector/MetricsCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lukonjun.metricscollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
