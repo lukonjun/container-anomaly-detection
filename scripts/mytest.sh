6ecf71c5e1cc464d56ef15158d16af9abcabe1f6
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2021-05-26 10:14:16 +0200

    improve script

--- scripts/mytest.sh
+++ scripts/mytest.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-function mytest {
-    "$@"
-    local status=$?
-    if (( status != 0 )); then
-        echo "error with $1" >&2
-    fi
-    return $status
-}
-mytest "kubectl get pods"
+# todo suppress output and onyl check error code
+
+echo -n "Do you want to install or uninstall charts?"
+read VAR
+
+if [[ "$VAR" == "install" ]]
+then
+  echo "install charts"
+elif [[ "$VAR" == "uninstall" ]]
+then
+  echo "uninstall charts"
+else
+  echo "No valid input"
+fi
--- scripts/update_helm_charts.sh
+++ scripts/update_helm_charts.sh
@@ -4,21 +4,14 @@ repo_array=(https://charts.bitnami.com/bitnami https://helm.wso2.com https://sto
 label_array=(mysql nginx)
 namespace=helm-charts
 
+echo -n "Do you want to install or uninstall charts?"
+read VAR
+
 if ! command -v helm &> /dev/null
 then
     echo "helm could not be found, please install it"
     exit 1
 fi
-
-echo "add helm repositories"
-total=5
-for (( i=0; i<=$(( $total -1 )); i++ ))
-do 
-    helm repo add "${name_array[$i]}" "${repo_array[$i]}"
-done
-
-echo "update helm repositories"
-helm repo update
 
 if ! command -v kubectl &> /dev/null
 then
@@ -26,57 +19,57 @@ then
     exit 1
 fi
 
-RESULT=$(command kubectl get ns | grep ${namespace})
-if [ "$RESULT" == "" ]
+### Install Part
+if [[ "$VAR" == "install" ]]
 then
-    echo "create namespace $namespace"
-    kubectl create namespace $namespace
+
+    echo "update helm repositories"
+    helm repo update
+
+    echo "add helm repositories"
+    total=5
+    for (( i=0; i<=$(( $total -1 )); i++ ))
+    do 
+        helm repo add "${name_array[$i]}" "${repo_array[$i]}"
+    done
+
+    RESULT=$(command kubectl get ns | grep ${namespace})
+    if [ "$RESULT" == "" ]
+    then
+        echo "create namespace $namespace"
+        kubectl create namespace $namespace
+    fi
+
+    lenght_name_array=${#name_array[@]}
+    lenght_label_array=${#label_array[@]}
+    for (( i=0; i<=$(( $lenght_name_array -1 )); i++ ))
+    do
+        for (( l=0; l<=$(( $lenght_label_array -1 )); l++ ))
+        do  
+            helm_search=$(helm search repo ${name_array[$i]}/${label_array[$l]})
+            if [ "$helm_search" != "No results found" ]
+            then
+                echo Running helm install --namespace $namespace release-${name_array[$i]}-${label_array[$l]} ${name_array[$i]}/${label_array[$l]}
+                command helm install --namespace $namespace release-${name_array[$i]}-${label_array[$l]} ${name_array[$i]}/${label_array[$l]} > /dev/null
+            fi
+        done
+    done
+
+### Uninstall Part
+elif [[ "$VAR" == "uninstall" ]]
+then
+
+    lenght_name_array=${#name_array[@]}
+    lenght_label_array=${#label_array[@]}
+    for (( i=0; i<=$(( $lenght_name_array -1 )); i++ ))
+    do
+        for (( l=0; l<=$(( $lenght_label_array -1 )); l++ ))
+        do  
+            echo Running helm uninstall --namespace $namespace release-${name_array[$i]}-${label_array[$l]}
+            command helm uninstall --namespace $namespace release-${name_array[$i]}-${label_array[$l]} > /dev/null 2>&1
+        done
+    done
+
+else
+  echo "No valid input, please specify install or uninstall"
 fi
-
-lenght_name_array=${#name_array[@]}
-lenght_label_array=${#label_array[@]}
-for (( i=0; i<=$(( $lenght_name_array -1 )); i++ ))
-do
-    for (( l=0; l<=$(( $lenght_label_array -1 )); l++ ))
-    do  
-        OUTPUT=$(ls -la)
-        test=$(helm search repo ${name_array[$i]}/${label_array[$l]})
-        echo $test
-        echo $OUTPUT
-        if  == "No results found"
-        then
-            echo "Hi"
-        fi
-    done  
-done
-
-wait
-# Mysql
-echo "install bitnami/mysql"
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install my-release bitnami/mysql
-echo "install wso2/mysql"
-helm repo add wso2 https://helm.wso2.com
-helm install my-mysql wso2/mysql --version 1.6.9
-echo "install wso2/mysql-am"
-helm repo add wso2 https://helm.wso2.com
-helm install my-mysql-am wso2/mysql-am --version 4.0.0-1
-echo "install t3n/mysql"
-helm repo add t3n https://storage.googleapis.com/t3n-helm-charts
-helm install my-mysql-t3n t3n/mysql --version 1.0.0
-
-# nginx
-helm repo add bitnami https://charts.bitnami.com/bitnami
-echo "install bitnami/nginx"
-helm install my-nginx-bitnami bitnami/nginx --version 8.9.0
-
-helm repo add mirantis https://charts.mirantis.com
-echo "install mirantis/nginx"
-helm install my-nginx-mirantis mirantis/nginx --version 0.1.0
-
-helm repo add shubhamtatvamasi https://shubhamtatvamasi.github.io/helm
-echo "install shubhamtatvamasi/nginx"
-helm install my-nginx-shubhamtatvamasi shubhamtatvamasi/nginx --version 0.1.12
-
-echo "install falco nginx"
-helm install t3n-nginx t3n/nginx
