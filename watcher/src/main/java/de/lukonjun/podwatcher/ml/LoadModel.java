60ff4c6f9783d698188f0ce65efece91d49c15cb
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2021-08-01 14:22:11 +0200

    template path for model

--- watcher/src/main/java/de/lukonjun/podwatcher/ml/LoadModel.java
+++ watcher/src/main/java/de/lukonjun/podwatcher/ml/LoadModel.java
@@ -2,6 +2,7 @@ package de.lukonjun.podwatcher.ml;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import weka.classifiers.trees.J48;
 
@@ -12,9 +13,8 @@ public class LoadModel {
 
     private J48 wekaModel;
 
-
-    public LoadModel() throws Exception {
-        this.wekaModel = (J48) weka.core.SerializationHelper.read("/tmp/weka_model");
+    public LoadModel(@Value("${path.serialized.model}") String pathSerializedModel) throws Exception {
+        this.wekaModel = (J48) weka.core.SerializationHelper.read(pathSerializedModel);
         logger.info("Loaded Model " +  this.wekaModel);
     }
 
